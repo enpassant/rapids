02928727e24ffb65de4079e0be440fd7fcf77348
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2017-01-28 20:50:30 +0100

    Add websocket updates

--- modules/web/src/main/scala/WebApp.scala
+++ modules/web/src/main/scala/WebApp.scala
@@ -6,6 +6,8 @@ import akka.kafka.scaladsl._
 import akka.persistence._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{UpgradeToWebSocket, TextMessage}
+import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling._
 import akka.stream._
@@ -39,19 +41,36 @@ object WebApp extends App {
 		.to(Producer.plainSink(producerSettings))
 		.run()
 
+	val consumerSettings = ConsumerSettings(
+		system,
+		new ByteArrayDeserializer,
+		new StringDeserializer
+	)
+		.withBootstrapServers("localhost:9092")
+		.withGroupId("webapp-1")
+		.withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+
+  val fromOffset = 0L
+  val subscription = Subscriptions.assignmentWithOffset(
+    new TopicPartition("accepted-commands", partition) -> fromOffset,
+    new TopicPartition("rejected-commands", partition) -> fromOffset
+  )
+	def process(consumerRecord: ConsumerRecord[Array[Byte], String]) = Future {
+		TextMessage(consumerRecord.toString)
+	}
+
+	def filter(clientId: String)
+		(consumerRecord: ConsumerRecord[Array[Byte], String]) =
+	{
+		new String(consumerRecord.key) == clientId
+	}
+
+	def consumer(clientId: String) =
+		Consumer.plainSource(consumerSettings, subscription)
+			.filter(filter(clientId))
+			.mapAsync(1)(process)
+
 	val route =
-		path("commands") {
-			post {
-				entity(as[String]) {
-					case "shutdown" =>
-						system.terminate
-						complete(
-							HttpEntity(
-								ContentTypes.`text/plain(UTF-8)`,
-								"System shut down"))
-					}
-			}
-		} ~
 		pathPrefix("commands") {
 			pathPrefix(Segment) { topic =>
 				path(Segment) { id =>
@@ -68,6 +87,38 @@ object WebApp extends App {
 					}
 				}
 			}
+		} ~
+		pathPrefix("updates") {
+			path(Segment) { id =>
+				optionalHeaderValueByType[UpgradeToWebSocket]() {
+					case Some(upgrade) =>
+						complete(
+							upgrade.handleMessagesWithSinkSource(Sink.ignore, consumer(id)))
+					case None =>
+						reject(ExpectedWebSocketRequestRejection)
+				}
+			}
+		} ~
+		path("system") {
+			post {
+				entity(as[String]) {
+					case "shutdown" =>
+						system.terminate
+						complete(
+							HttpEntity(
+								ContentTypes.`text/plain(UTF-8)`,
+								"System shut down"))
+					}
+			}
+		} ~
+		path("") {
+			getFromResource(s"public/html/index.html")
+		} ~
+		path("""([^/]+\.html).*""".r) { path =>
+			getFromResource(s"public/html/$path")
+		} ~
+		path(Remaining) { path =>
+			getFromResource(s"public/$path")
 		}
 
 	val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
