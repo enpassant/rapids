9bb56f14ce3b2069dbab1a48f5964aa8e1755c0b
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2017-03-07 17:02:21 +0100

    Move authentications into web-common module

--- modules/web-common/src/main/scala/common/web/Directives.scala
+++ modules/web-common/src/main/scala/common/web/Directives.scala
@@ -1,5 +1,6 @@
 package common.web
 
+import auth._
 import common._
 
 import akka.actor._
@@ -7,12 +8,16 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Accept
+import akka.http.scaladsl.server.directives.Credentials
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream._
+import java.util.Base64
 import com.github.jknack.handlebars.{ Context, Handlebars }
 import com.mongodb.casbah.Imports._
 import com.typesafe.config.ConfigFactory
+import org.json4s._
+import org.json4s.jackson.JsonMethods.{parse => jparse}
 import org.json4s.JsonAST._
 import org.json4s.JsonDSL._
 import org.json4s.mongo.JObjectParser._
@@ -46,4 +51,46 @@ object Directives extends BaseFormats {
       }.getOrElse(reject)
     }
   }
+
+  val encoder = Base64.getEncoder()
+  val decoder = Base64.getDecoder()
+
+  def authenticateJwt(credentials: Credentials): Option[LoggedIn] = {
+    credentials match {
+      case Credentials.Provided(id) =>
+        val parts = id.split('.')
+        if (parts.length == 3) {
+          val header = parts(0)
+          CommonUtil.encodeOpt("secret", s"$header.${parts(1)}") { t =>
+            if (encoder.encodeToString(t) == parts(2)) {
+              implicit val formats = DefaultFormats
+              val json = jparse(new String(decoder.decode(parts(1))))
+                .extract[Payload]
+              if (System.currentTimeMillis / 1000 <= json.exp) {
+                CommonUtil.createJwt(json.sub, 5 * 60)
+              } else {
+                None
+              }
+            } else {
+              None
+            }
+          }
+        } else {
+          None
+        }
+      case _ => None
+    }
+  }
+
+  def authenticate(credentials: Credentials): Option[LoggedIn] = {
+    credentials match {
+      case p @ Credentials.Provided(id) if p.verify(id) =>
+        CommonUtil.createJwt(id, 5 * 60)
+      case _ => None
+    }
+  }
+
+  val authenticates =
+    authenticateOAuth2("rapids", authenticateJwt) |
+      authenticateBasic(realm = "rapids", authenticate)
 }
--- modules/web/src/main/scala/WebApp.scala
+++ modules/web/src/main/scala/WebApp.scala
@@ -1,29 +1,20 @@
-import common._
 import auth._
+import common._
+import common.web.Directives._
 
 import akka.actor._
 import akka.kafka._
 import akka.kafka.ConsumerMessage._
 import akka.kafka.scaladsl._
-import akka.persistence._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.model.ws.{UpgradeToWebSocket, TextMessage}
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.directives.Credentials
-import akka.http.scaladsl.unmarshalling._
 import akka.stream._
 import akka.stream.scaladsl._
-import java.util.Base64
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.TopicPartition
-import org.apache.kafka.common.serialization._
-import org.json4s._
-import org.json4s.jackson.JsonMethods._
 import scala.concurrent.Future
 
 object WebApp extends App {
@@ -51,48 +42,6 @@ object WebApp extends App {
 		def consumer(clientId: String) = consumerSource.filter {
       msg => msg._1 == clientId
     }.map(_._2)
-
-    val encoder = Base64.getEncoder()
-    val decoder = Base64.getDecoder()
-
-    def authenticateJwt(credentials: Credentials): Option[LoggedIn] = {
-      credentials match {
-        case Credentials.Provided(id) =>
-          val parts = id.split('.')
-          if (parts.length == 3) {
-            val header = parts(0)
-            CommonUtil.encodeOpt("secret", s"$header.${parts(1)}") { t =>
-              if (encoder.encodeToString(t) == parts(2)) {
-                implicit val formats = DefaultFormats
-                val json = parse(new String(decoder.decode(parts(1))))
-                  .extract[Payload]
-                if (System.currentTimeMillis / 1000 <= json.exp) {
-                  CommonUtil.createJwt(json.sub, 5 * 60)
-                } else {
-                  None
-                }
-              } else {
-                None
-              }
-            }
-          } else {
-            None
-          }
-        case _ => None
-      }
-    }
-
-    def authenticate(credentials: Credentials): Option[LoggedIn] = {
-      credentials match {
-        case p @ Credentials.Provided(id) if p.verify(id) =>
-          CommonUtil.createJwt(id, 5 * 60)
-        case _ => None
-      }
-    }
-
-    val authenticates =
-      authenticateOAuth2("rapids", authenticateJwt) |
-        authenticateBasic(realm = "rapids", authenticate)
 
 		val route =
 			pathPrefix("commands") {
