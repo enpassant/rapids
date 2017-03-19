29e4cd307a07f34db4bd1b1d9e7417d115424b0f
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2017-03-19 20:09:00 +0100

    Complete RealGatling

--- src/it/scala/RealGatling.scala
+++ src/it/scala/RealGatling.scala
@@ -7,6 +7,7 @@ class RealGatling extends Simulation {
   val feeder = Iterator.continually(
     Map(
       "userId" -> Random.nextInt(100).toString,
+      "blogId" -> Random.nextInt(100).toString,
       "uuid" -> common.CommonUtil.uuid
     )
   )
@@ -14,23 +15,31 @@ class RealGatling extends Simulation {
   val scn = scenario("RealGatling")
     .feed(feeder)
     .exec(
-      http("login").post("/login").basicAuth(s"john$${userId}", s"john$${userId}")
-      .check(
-        header("X-Token").exists.saveAs("token"))
+      http("login")
+        .post("/login")
+        .basicAuth(s"john$${userId}", s"john$${userId}")
+        .check(header("X-Token").exists.saveAs("token"))
     )
-    .repeat(1, "blogId") {
-      exec(Command.createBlog)
-    }
-    .pause(900 milliseconds)
-    .repeat(1, "blogId") {
-      exec(Command.queryBlogs)
-      .repeat(5, "commentId") {
-        exec(Command.addComment)
-        //.pause(10 milliseconds)
-        //.repeat(5, "replyId") {
-          //exec(Command.replyComment)
-        //}
+    .exec(Command.queryBlogs)
+    .doIfOrElse(session => session("blogCount").as[Int] >= 200) {
+      repeat(50, "commentId") {
+        exec { session =>
+          val ids = session("discussionIds").as[Seq[String]]
+          session.set("discussionUuid",
+            ids(scala.util.Random.nextInt(ids.length)))
+        }
+        .exec(Command.queryComments)
+        .repeat(5, "replyId") {
+          doIfOrElse(session => session.contains("commentIds")) {
+            exec(Command.replyComment)
+          } {
+            exec(Command.addComment)
+          }
+        }
+        .pause(1)
       }
+    } {
+      exec(Command.createBlog)
     }
 
   val httpConf = http
@@ -42,33 +51,44 @@ class RealGatling extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
   setUp(
-    scn.inject(rampUsersPerSec(1) to(10) during(5 seconds) randomized)
+    scn.inject(rampUsersPerSec(1) to(15) during(50 seconds) randomized)
   ).protocols(httpConf)
 
   object Command {
-    val queryBlogs = http("QueryBlogs")
+    val queryBlogs = http("queryBlogs")
       .get("/blog")
       .header("Authorization", s"Bearer $${token}")
       .header(HttpHeaderNames.Accept, HttpHeaderValues.ApplicationJson)
       .check(status.is(session => 200))
-      .check(jsonPath("$.blogs[*]._id").findAll.saveAs("blogIds"))
+      .check(jsonPath("$.blogs[*]._id").count.optional.saveAs("blogCount"))
+      .check(jsonPath("$.blogs[*].discussions[*].id").findAll.optional
+        .saveAs("discussionIds"))
 
-    val createBlog = http("CreateBlog")
+    val queryComments = http("queryComments")
+      .get(s"/discussion/$${discussionIds.random()}")
+      .header("Authorization", s"Bearer $${token}")
+      .header(HttpHeaderNames.Accept, HttpHeaderValues.ApplicationJson)
+      .check(status.is(session => 200))
+      .check(jsonPath("$._id").exists.saveAs("discussionUuid"))
+      .check(jsonPath("$..commentId").findAll.optional
+        .saveAs("commentIds"))
+
+    val createBlog = http("createBlog")
       .post(s"/commands/blog/$${uuid}")
       .header("Authorization", s"Bearer $${token}")
       .body(StringBody(s"""{"_t":"CreateBlog", "title": "Blog $${userId} $${blogId}", "content": "My $${userId} $${blogId}. blog", "loggedIn": ""}"""))
       .check(status.is(session => 200))
 
-    val addComment = http("AddComment")
-      .post(s"/commands/discussion/$${blogIds.random()}")
+    val addComment = http("addComment")
+      .post(s"/commands/discussion/$${discussionUuid}")
       .header("Authorization", s"Bearer $${token}")
-      .body(StringBody(s"""{"_t":"AddComment", "id": "$${userId}$${blogId}-$${commentId}", "content": "$${commentId}. megjegyzés", "loggedIn": ""}"""))
+      .body(StringBody(s"""{"_t":"AddComment", "id": "$${uuid}", "content": "$${commentId}. megjegyzés", "loggedIn": ""}"""))
       .check(status.is(session => 200))
 
-    val replyComment = http("ReplyComment")
-      .post(s"/commands/discussion/disc-$${userId}$${blogId}")
+    val replyComment = http("replyComment")
+      .post(s"/commands/discussion/$${discussionUuid}")
       .header("Authorization", s"Bearer $${token}")
-      .body(StringBody(s"""{"_t":"ReplyComment", "id": "$${userId}$${blogId}-$${commentId}-$${replyId}", "parentId": "$${userId}$${blogId}-$${commentId}", "content": "$${replyId}. válasz", "loggedIn": ""}"""))
+      .body(StringBody(s"""{"_t":"ReplyComment", "id": "$${uuid}", "parentId": "$${commentIds.random()}", "content": "$${replyId}. válasz", "loggedIn": ""}"""))
       .check(status.is(session => 200))
   }
 }
