f4d9585db7869596419d4485421cf3286eb4d477
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2017-05-28 18:30:53 +0200

    Exit when pressing ENTER in test mode

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,14 +7,11 @@ import akka.stream._
 import scala.util.{Failure, Success}
 
 object Main extends App {
+  val isTest = args.length > 0 && args(0) == "-t"
 	implicit val system = ActorSystem("Main")
 	implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
-  implicit val mq = if (args.length > 0 && args(0) == "-t") {
-    new MQTest(system)
-  } else {
-    Kafka
-  }
+  implicit val mq = if (isTest) new MQTest(system) else Kafka
 
   println("Program has started")
 
@@ -37,5 +34,11 @@ object Main extends App {
       println("Failed to bind!")
     case Success(t) =>
       println(s"Successed to bind to $t")
+  }
+
+  if (isTest) {
+    println("Press ENTER to exit!")
+    scala.io.StdIn.readLine()
+    system.terminate()
   }
 }
